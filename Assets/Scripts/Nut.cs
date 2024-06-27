3ab034b9ddfb3583c69ed4cf58bb525a73429588
usman <usmanahmed.cap@gmail.com>
2024-06-27 19:18:55 +0500

    Day (1)

--- Assets/Scripts/Nut.cs
+++ Assets/Scripts/Nut.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Nut : MonoBehaviour
@@ -16,11 +17,22 @@ public class Nut : MonoBehaviour
     [SerializeField]
     private NutsCollection collection;
 
+    /*[SerializeField]
+    private Animator animator;*/
+
+    public bool isNutOpened = false;
+    bool completed = true;
     // Start is called before the first frame update
     void Start()
     {
         color = collection.colors[(int)nutCol];
         mRender.material.color = color;
+        //animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+
     }
 
     // Update is called once per frame
@@ -28,26 +40,85 @@ public class Nut : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+
+            if (collection.IsNutOpened() && openedNut && openedNut.isNutOpened && completed)
+            {
+                if (openedNut != null)
+                {
+                    openedNut.isNutOpened = false;
+                    //openedNut.animator.enabled = true;
+                    //openedNut.animator.SetBool("CloseNut", true);
+                    CloseNut();
+                }
+            }
+
+
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
             {
-                if(hit.collider != null)
+                if (hit.collider != null)
                 {
-                    if(hit.collider.gameObject.name.Contains("Nut") || hit.collider.gameObject.name.Contains("Bolt"))
+                    if (hit.collider.gameObject.name.Contains("nut") || hit.collider.gameObject.name.Contains("bolt"))
                     {
-                        /*if (collection.isNutOpened)
+                        if (!collection.IsNutOpened() && !isNutOpened && completed)
                         {
-                            collection.isNutOpened = false;
-                        }*/
-
-                        if (!collection.isNutOpened)
-                        {
-                            collection.isNutOpened = true;
+                            collection.SetNutOpened(true);
+                            if (collection.bolt.nuts.Count > 0)
+                            {
+                                if (collection.bolt.nuts.TryPop(out var nut1))
+                                {
+                                    //var nut1 = hit.collider.GetComponentInParent<Nut>();
+                                    if (nut1 != null)
+                                    {
+                                        nut1.isNutOpened = true;
+                                        Debug.Log(nut1.name);
+                                        //nut1.animator.enabled = true;
+                                        //nut1.animator.SetBool("OpenNut", true);
+                                        OpenNut(nut1);
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
         }
+
+    }
+
+
+    private Nut openedNut;
+    private Vector3 nutInitialPosition = Vector3.zero;
+    void OpenNut(Nut nut)
+    {
+        nutInitialPosition = nut.transform.localPosition;
+        openedNut = nut;
+        completed = false;
+        nut.transform.DORotate(new Vector3(0,-113,0), 1f).SetEase(Ease.Linear);
+        nut.transform.DOLocalMoveY(0.0645f, 1f).SetEase(Ease.Linear).SetUpdate(true).OnComplete(() =>
+        {
+            completed = true;
+            //nut.animator.SetBool("OpenNut", false);
+            //openedNut.animator.Rebind();
+            //openedNut.animator.enabled = false;
+        });
+    }
+
+
+    void CloseNut()
+    {
+        completed = false;
+        openedNut.transform.DORotate(Vector3.zero, 1f).SetEase(Ease.Linear);
+        openedNut.transform.DOLocalMoveY(nutInitialPosition.y, 1f).SetEase(Ease.Linear).SetUpdate(true).OnComplete(() =>
+        {
+            completed = true;
+            collection.bolt.nuts.Push(openedNut);
+            //openedNut.animator.SetBool("CloseNut", false);
+            //openedNut.animator.Rebind();
+            //openedNut.animator.enabled = false;
+            collection.SetNutOpened(false);
+            openedNut = null;
+        });
     }
 }
--- Assets/Scripts/NutsCollection.cs
+++ Assets/Scripts/NutsCollection.cs
@@ -1,22 +1,44 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NutsCollection : MonoBehaviour
 {
     public List<Color> colors = new List<Color>();
-
+    public Bolt bolt;
     public bool isNutOpened;
+
+    [SerializeField]
+    private List<Nut> nuts = new List<Nut>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddNutsIntoStack();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    void AddNutsIntoStack()
+    {
+        foreach (var nut in nuts)
+        {
+            bolt.nuts.Push(nut);
+        }
+    }
+
+    public bool IsNutOpened()
+    {
+        return isNutOpened;
+    }
+
+    public void SetNutOpened(bool status)
+    {
+        isNutOpened = status;
     }
 }
